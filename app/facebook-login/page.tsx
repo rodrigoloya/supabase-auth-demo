99360f16d743f894523807891f2351fe21ace74c
RloyaDesktop <al_corp@hotmail.com>
2026-06-09 22:04:40 -0400

    feat: add Facebook login demonstration with OAuth integration

---
+++ app/facebook-login/page.tsx
@@ -0,0 +1,10 @@
+import FacebookLoginPageDemo from "./FacebookLoginDemo";
+
+
+
+export default function FacebookLoginPage() {
+    return (
+       <FacebookLoginPageDemo user={null} />
+
+    );
+}
